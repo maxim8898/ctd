0b4764f8042bdbf93a21a2da90f8dc61fbf167c7
Maksim Matuzka <maksim.matuzka@innowise.com>
2024-09-04 17:15:54 +0300

    Init commit

--- src/index.tsx
+++ src/index.tsx
@@ -2,18 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import {Button, Typography} from "@mui/material";
 import reportWebVitals from './reportWebVitals';
+import {TypeSpecimen} from "@mui/icons-material";
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <React.StrictMode>
+        <Typography className="text-3xl font-bold underline">
+            Hello world!
+        </Typography>
+    </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
